e0952bd6b59c6fc075b573489b011c2818e28ac9
JuanDulcey <juanedulcey05@gmail.com>
2025-08-12 23:36:18 -0500

    Initial commit: DevVital Health Engine skeleton

---
+++ src/main/java/com/devv/devvital/DevVitalHealthEngineApplication.java
@@ -0,0 +1,13 @@
+package com.devv.devvital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevVitalHealthEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevVitalHealthEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devv/devvital/DevVitalHealthEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devv.devvital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevVitalHealthEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
